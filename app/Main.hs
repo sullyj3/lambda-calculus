ccf4c52831bdce8a18d42aab8a70ba0d5a8a5b1f
James Sully <sullyj3@gmail.com>
2022-04-08 08:50:49 +1000

    Add repl that prints parsed AST

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
 import Lib
+import Text.Megaparsec (parse)
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+import System.IO (hFlush, stdout)
 
 main :: IO ()
-main = putStrLn "Hello"
+main = let
+
+  prompt :: Text -> IO Text
+  prompt s = do
+    T.putStr s
+    hFlush stdout
+    T.getLine
+
+  loop = do
+    line <- prompt "> "
+    case parse expr "interpreter" line of 
+      Right e  -> print e
+      Left err -> T.putStrLn "Parse failed"
+    loop
+
+  in do
+    T.putStrLn "Lambda calculus interpreter"
+    loop
