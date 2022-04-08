36484893c139478c3b811f3d0284e11d563168f5
James Sully <sullyj3@gmail.com>
2022-04-08 10:14:26 +1000

    Evaluation seems to work

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,36 +1,36 @@
 module Eval where
 
 -- TODO these should be in seperate module
-import Lib (Expr(..), Variable(..))
+import Lib (Expr (..), Variable (..))
 
 eval :: Expr -> Expr
-eval
-  = \case
-      -- Beta normal form
-      abstr@(Abstraction _ _) -> abstr
-      -- Beta normal form
-      v@(Var _) -> v
-      app@(Application (Var _) _) -> app
-      Application (Abstraction paramVar body) arg -> 
-        betaReduce (eval arg) paramVar (eval body)
-      Application _ _ -> undefined
+eval =
+  \case
+    (Abstraction v body) -> Abstraction v (eval body)
+    v@(Var _) -> v
+    Application fn arg ->
+      let fn' = eval fn
+          arg' = eval arg
+       in case fn' of
+            Abstraction paramVar body ->
+              eval $ betaReduce arg' paramVar body
+            _ -> Application fn' arg'
 
 -- TODO refactor to use these
 newtype Argument = Argument Expr
+
 newtype Body = Body Expr
 
 betaReduce :: Expr -> Variable -> Expr -> Expr
-betaReduce arg paramVar = let 
-  go = betaReduce arg paramVar
-  in 
-  \case
-    abstr@(Abstraction innerParamVar body) 
-      -- The inner abstraction shadows the argument we are substituting,
-      -- so we should not continue substituting in its body
-      | paramVar == innerParamVar -> abstr
-      | otherwise -> Abstraction innerParamVar (go body)
-    Var v
-      | paramVar == v -> arg
-      | otherwise -> (Var v)
-    Application fn arg' -> Application (go fn) (go arg')
-
+betaReduce arg paramVar =
+  let go = betaReduce arg paramVar
+   in \case
+        abstr@(Abstraction innerParamVar body)
+          -- The inner abstraction shadows the argument we are substituting,
+          -- so we should not continue substituting in its body
+          | paramVar == innerParamVar -> abstr
+          | otherwise -> Abstraction innerParamVar (go body)
+        Var v
+          | paramVar == v -> arg
+          | otherwise -> (Var v)
+        Application fn arg' -> Application (go fn) (go arg')
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,8 +5,8 @@ import Eval
 import Text.Megaparsec
 import Data.Text (Text)
 
-parserSucceeds :: (Show a, Eq a) => Parser a -> Text -> a -> Expectation
-parserSucceeds parser input expected =
+parsesAs :: (Show a, Eq a) => Parser a -> Text -> a -> Expectation
+parsesAs parser input expected =
   parseMaybe parser input `shouldBe` Just expected
 
 main :: IO ()
@@ -16,43 +16,43 @@ main = hspec do
 
 testExprParser :: SpecWith ()
 testExprParser = describe "expression parser" do
-  let exprSucceeds = parserSucceeds expr
+  let exprParsesAs = parsesAs expr
 
   it "parses a single variable" do
-    "a" `exprSucceeds` Var (Variable 'a')
+    "a" `exprParsesAs` Var (Variable 'a')
 
   it "parses the identity function" do
-    "\\x.x" `exprSucceeds` Abstraction (Variable 'x') (Var $ Variable 'x')
+    "\\x.x" `exprParsesAs` Abstraction (Variable 'x') (Var $ Variable 'x')
 
   it "parses an application of a variable to another" do
-    "ab" `exprSucceeds` Application (Var $ Variable 'a') (Var $ Variable 'b')
+    "ab" `exprParsesAs` Application (Var $ Variable 'a') (Var $ Variable 'b')
 
   it "parses an application of the identity function to a variable" do
     let expected = Application (Var $ Variable 'a') (Var $ Variable 'b')
-    "ab" `exprSucceeds` expected
+    "ab" `exprParsesAs` expected
 
   it "parses an application of an application" do
     let expected = Application (Application (Var $ Variable 'a') (Var $ Variable 'b')) (Var $ Variable 'c')
-    "abc" `exprSucceeds` expected
+    "abc" `exprParsesAs` expected
 
   it "parses a single variable in parens" do
     let expected = Var $ Variable 'x'
-    "(x)" `exprSucceeds` expected
+    "(x)" `exprParsesAs` expected
 
   it "parses an application of a variable in parens to a variable" do
     let expected = Application (Var $ Variable 'x') (Var $ Variable 'y')
-    "(x)y" `exprSucceeds` expected
+    "(x)y" `exprParsesAs` expected
 
   it "parses a triple application involving parens (1)" do
     let expected = Application (Application (Var $ Variable 'x') (Var $ Variable 'y')) (Var $ Variable 'z')
-    "(x)y(z)" `exprSucceeds` expected
+    "(x)y(z)" `exprParsesAs` expected
 
   it "parses an application of a variable to a parenthesized expression" do
     let expected = Application
                      (Var $ Variable 'e')
                      (Application (Var $ Variable 'f')
                                   (Var $ Variable 'g'))
-    "e(fg)" `exprSucceeds` expected
+    "e(fg)" `exprParsesAs` expected
 
 parseUnsafeUnwrap :: Parser a -> Text -> a
 parseUnsafeUnwrap p s = let 
@@ -76,3 +76,13 @@ testEvaluator = describe "evaluator" do
   it "does not substitute shadowed variables" do
     "(\\x.(\\x.xa))z" `reducesTo` "(\\x.xa)"
     "(\\x.(\\x.xa)x)z" `reducesTo` "za"
+
+  it "evaluates application of application to variable" do
+    "(ab)c" `reducesTo` "abc"
+    "((\\x.x)b)c" `reducesTo` "bc"
+
+  it "evaluates abstraction containing reducible body" do
+    "\\x.(\\y.y)z" `reducesTo` "\\x.z"
+
+  it "further reduces if possible when substituting an abstraction" do
+    "(\\x.xz)(\\x.x)" `reducesTo` "z"
